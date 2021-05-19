23b6cd9a7cca3e6fcce85dbb8256dc266dbf0214
Pavlo Mryhlotskyi <kozak.holota@gmail.com>
2021-05-19 22:52:26 +0300

    Complete version

---
+++ SageBookUI/SgDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SageBookUI
+{
+    public class SgDbContext : DbContext
+    {
+        public SgDbContext(): base("conStr")
+        {
+        }
+    }
+}
